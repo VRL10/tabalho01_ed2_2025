28144715e9cfaf333a9277f2eaf6410c7dd8d0b7
Victor Rodrigues Luz <130073068+VRL10@users.noreply.github.com>
2025-03-10 14:00:31 -0300

    coloquei o enunciado da Q1

--- arvore.h
+++ arvore.h
@@ -1,6 +1,13 @@
 #ifndef ARVORE_H
 #define ARVORE_H
 
+/*Faça um programa em C de uma Biblioteca de Música. As informações são organizadas por
+Artista. E para cada artista deve ser ter o nome do artista, tipo (cantor(a), Dupla, Banda, Grupo, …), o estilo
+musical, o número de álbuns, e os Álbuns (endereço da árvore binária para Álbuns). Cada álbum deve ter,
+o título, o ano de lançamento, a quantidade de músicas e as Músicas (endereço da árvore binária para
+Músicas). Para cada música deve se ter o título, e a quantidade de minutos. Quando o usuário executar o
+programa o mesmo deve permitir:*/
+
 typedef struct arvore_artista{ // organizada pelo nome
     char nome[50],tipo[20],estilo_musical[50];
     int numero_albuns;
